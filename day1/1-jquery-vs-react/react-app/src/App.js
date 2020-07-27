578a82e2fb644ea76cf0569690050a4081cdf2de
Mehmet Seven <mehmetseven0@gmail.com>
2020-07-27 14:33:54 +0300

    day1

---
+++ day1/1-jquery-vs-react/react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<h1>App Component</h1>
+		</div>
+	);
+}
+
+export default App;
