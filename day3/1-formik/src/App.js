c88e51a37ecc5c83423ec726f6b3f9330d9a468a
Mehmet Seven <mehmetseven0@gmail.com>
2020-07-29 14:43:05 +0300

    formik basics

---
+++ day3/1-formik/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import UserForm from "./components/UserForm";
+
+function App() {
+	return (
+		<>
+			<UserForm />
+		</>
+	);
+}
+
+export default App;
